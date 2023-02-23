1d75c0942fc583cf66d7f5c705b560c8208eae51
Alexander_G <alexander.4ephb@gmail.com>
2023-02-24 02:16:27 +1000

    HW04 added

---
+++ HW04/db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE AI_GROUP (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  address TEXT NOT NULL
+);
+
+INSERT INTO AI_GROUP VALUES (0001, 'Иван', 33, 'Ростов-на-Дону');
+INSERT INTO AI_GROUP VALUES (0002, 'Наталья', 36, 'Салават');
+INSERT INTO AI_GROUP VALUES (0003, 'Александр', 32, 'Ростов-на-Дону');
+INSERT INTO AI_GROUP VALUES (0004, 'Ростислав', 30, 'Екатеринбург');
+INSERT INTO AI_GROUP VALUES (0005, 'Игорь', 38, 'Чемал');
+INSERT INTO AI_GROUP VALUES (0006, 'Полина', 30, 'Москва');
+INSERT INTO AI_GROUP VALUES (0007, 'Ольга', 30, 'Санкт-Петербург');
+INSERT INTO AI_GROUP VALUES (0008, 'Евгений', 48, 'Магнитогорск');
+INSERT INTO AI_GROUP VALUES (0009, 'Ольга', 43, 'Москва');
+INSERT INTO AI_GROUP VALUES (0010, 'Александр', 41, 'Севастополь');
+INSERT INTO AI_GROUP VALUES (0011, 'Ярослав', 35, 'Тольятти');
